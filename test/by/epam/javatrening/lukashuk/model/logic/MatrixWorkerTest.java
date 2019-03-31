840aa0a1b7be19af84b29aa5e98e5eb9d6935d44
KaterinaLukashuk <46955141+KaterinaLukashuk@users.noreply.github.com>
2019-03-31 21:46:22 +0300

    Add files via upload

--- test/by/epam/javatrening/lukashuk/model/logic/MatrixWorkerTest.java
+++ test/by/epam/javatrening/lukashuk/model/logic/MatrixWorkerTest.java
@@ -7,9 +7,9 @@ import static org.junit.Assert.*;
 
 public class MatrixWorkerTest {
 
-    @Test
-    public void getMax() {
-        Matrix matrix = new Matrix(4 ,4);
+    Matrix matrix;
+    {
+        matrix = new Matrix(4 ,4);
         matrix.add(0,0, 1);
         matrix.add(0,1, 2);
         matrix.add(0,2, 2);
@@ -26,31 +26,21 @@ public class MatrixWorkerTest {
         matrix.add(3,1, 2);
         matrix.add(3,2, 2);
         matrix.add(3,3, 2);
+
+    }
+
+    @Test
+    public void getMax() {
+
         int expected = 8;
         assertEquals(expected, MatrixWorker.getMax(matrix), 0.01);
     }
 
     @Test
     public void isSymmetric() {
-        Matrix matrix = new Matrix(4 ,4);
-        matrix.add(0,0, 1);
-        matrix.add(0,1, 2);
-        matrix.add(0,2, 2);
-        matrix.add(0,3, 2);
-        matrix.add(1,0, 1);
-        matrix.add(1,1, 2);
-        matrix.add(1,2, 2);
-        matrix.add(1,3, 2);
-        matrix.add(2,0, 1);
-        matrix.add(2,1, 8);
-        matrix.add(2,2, 2);
-        matrix.add(2,3, 2);
-        matrix.add(3,0, 1);
-        matrix.add(3,1, 2);
-        matrix.add(3,2, 2);
-        matrix.add(3,3, 2);
         boolean expected = false;
         assertEquals(expected, MatrixWorker.isSymmetric(matrix));
     }
+
 
 }
--- test/by/epam/javatrening/lukashuk/model/logic/VectorWorkerTest.java
+++ test/by/epam/javatrening/lukashuk/model/logic/VectorWorkerTest.java
@@ -8,38 +8,37 @@ import static org.junit.Assert.*;
 
 public class VectorWorkerTest {
 
-    @Test
-    public void getMax() {
-        Vector vector = new Vector();
+        Vector vector;
+        Vector sortedVector;
+    {
+        vector = new Vector();
         vector.add(1);
         vector.add(4);
         vector.add(2);
         vector.add(3);
         vector.add(0);
+        sortedVector = new Vector();
+        sortedVector.add(0);
+        sortedVector.add(1);
+        sortedVector.add(2);
+        sortedVector.add(3);
+        sortedVector.add(4);
+    }
+    @Test
+    public void getMax() {
+
         double expected = 4;
         assertEquals(expected, VectorWorker.getMax(vector),0.01);
     }
 
     @Test
     public void getMin() {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(4);
-        vector.add(2);
-        vector.add(3);
-        vector.add(0);
         double expected = 0;
         assertEquals(expected, VectorWorker.getMin(vector),0.01);
     }
 
     @Test
     public void getAverage() {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(4);
-        vector.add(2);
-        vector.add(3);
-        vector.add(0);
         double expected = 2;
         assertEquals(expected, VectorWorker.getAverage(vector),0.01);
     }
@@ -51,36 +50,18 @@ public class VectorWorkerTest {
 
     @Test
     public void isSorted() {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(4);
-        vector.add(2);
-        vector.add(3);
-        vector.add(0);
         boolean expected = false;
         assertEquals(expected, VectorWorker.isSorted(vector));
     }
 
     @Test
     public void firstLocalMinIndex() {
-        Vector vector = new Vector();
-        vector.add(6);
-        vector.add(4);
-        vector.add(2);
-        vector.add(3);
-        vector.add(0);
-        int expected = 2;
+        int expected = 0;
         assertEquals(expected, VectorWorker.getFirstLocalMinIndex(vector));
     }
 
     @Test
     public void revers() {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(4);
-        vector.add(2);
-        vector.add(3);
-        vector.add(0);
         Vector expected = new Vector();
         expected.add(0);
         expected.add(3);
@@ -92,63 +73,28 @@ public class VectorWorkerTest {
 
     @Test
     public void qSort() {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(4);
-        vector.add(2);
-        vector.add(3);
-        vector.add(0);
-        Vector expected = new Vector();
-        expected.add(0);
-        expected.add(1);
-        expected.add(2);
-        expected.add(3);
-        expected.add(4);
         VectorWorker.qSort(vector , 0, vector.length() -1);
-        assertArrayEquals(expected.getArray(),vector.getArray(),0.01);
+        assertArrayEquals(sortedVector.getArray(),vector.getArray(),0.01);
     }
 
     @Test
     public void bubbleSort() {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(4);
-        vector.add(2);
-        vector.add(3);
-        vector.add(0);
-        Vector expected = new Vector();
-        expected.add(0);
-        expected.add(1);
-        expected.add(2);
-        expected.add(3);
-        expected.add(4);
         VectorWorker.bubbleSort(vector);
-        assertArrayEquals(expected.getArray(),vector.getArray(),0.01);
-
+        assertArrayEquals(sortedVector.getArray(),vector.getArray(),0.01);
     }
 
     @Test
     public void binarySearch() {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(2);
-        vector.add(3);
-        vector.add(4);
-        vector.add(5);
-        double element = 4;
+
+        double element = 3;
         int expected = 3;
-        assertEquals(expected, VectorWorker.binarySearch(vector, element));
+        assertEquals(expected, VectorWorker.binarySearch(sortedVector, element));
     }
 
     @Test
     public void lineSearch() {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(4);
-        vector.add(5);
-        vector.add(1);
-        vector.add(9);
-        double element = 5;
+
+        double element = 2;
         int expected = 2;
         assertEquals(expected, VectorWorker.lineSearch(vector, element));
     }
